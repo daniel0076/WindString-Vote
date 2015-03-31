11be069110ad99a79b75c2b0a2e7bfb16c571f78
Daniel Tsai <idaniel.twc@gmail.com>
2015-04-01 01:50:19 +0800

    voting system done

--- vote.php
+++ vote.php
@@ -1,7 +1,16 @@
 <?php
 session_start();
-require('../vendor/autoload.php');
+
+if(isset($_GET['cid'])){
+    $cid=$_GET['cid'];
+    if(!is_numeric($cid))die();
+}else die();
+
+require('vendor/autoload.php');
 require_once('admin/auth/fb.php');
+require_once('admin/auth/db.php');
+require_once('static/xajax-0.6-beta1/xajax_core/xajax.inc.php');
+date_default_timezone_set('Asia/Taipei');
 
 use Facebook\FacebookSession;
 use Facebook\FacebookRequest;
@@ -10,11 +19,60 @@ use Facebook\FacebookRequestException;
 use Facebook\FacebookRedirectLoginHelper;
 
 FacebookSession::setDefaultApplication($app_id,$app_secret);
-$redirect_url="http://guitar.nctucs.net/windstring/admin/fb_auth.php";
+$redirect_url="http://guitar.nctucs.net/windstring/vote.php?cid=$cid";
 $helper = new FacebookRedirectLoginHelper($redirect_url);
 $session = $helper->getSessionFromRedirect();
+$user_profile=fb_auth($session,$helper);
+vote($user_profile);
 
-function fb_auth($session){
+function vote($user_profile){
+    global $db;
+    if($user_profile){
+        $query="SELECT * FROM `voters` WHERE `fb_id` = :fb_id";
+        try{
+            $dbe=$db->prepare($query);
+            $query_array=array(':fb_id'=>$user_profile->getId());
+            $dbe->execute($query_array);
+            $result=$dbe->fetch(PDO::FETCH_ASSOC);
+        }
+        catch(PDOException $e){
+            echo "DB Error".$e;
+            die();
+        }
+
+        if($result){
+            echo "<script>alert('你已經投過了ㄛQQ');location.href='/windstring/'</script>";
+            die();
+        }else{
+            $cid=$_GET['cid'];
+            $ip=get_ip();
+            $_date=date('Y-m-d H:i:s');
+            $query="INSERT INTO `voters` (`vid`, `name`, `gender`, `email`, `fb_id`, `cid`, `from_ip`, `date`) VALUES (NULL, :name, :gender, :email, :fb_id, :cid, :from_ip, :_date)";
+            $dbe->execute($query_array);
+            try{
+                $dbe=$db->prepare($query);
+                $query_array=array(':name'=>$user_profile->getName(),':gender'=>$user_profile->getGender(),':email'=>$user_profile->getEmail(),':fb_id'=>$user_profile->getId(),':cid'=>$cid,':from_ip'=>$ip,':_date'=>$_date);
+                $dbe->execute($query_array);
+            }
+            catch(PDOException $e){
+                echo "<script>alert('資料庫錯誤，請再試一次');location.href='/windstring/'</script>";
+                die();
+            }
+            echo "<script>alert('感謝你的投票');location.href='/windstring/'</script>";
+        }
+
+    }else die();
+}
+function get_ip(){
+    if(getenv('HTTP_X_FORWARDED_FOR')){
+        $ip=getenv('HTTP_X_FORWARDED_FOR');
+    }else if(getenv('REMOTE_ADDR')){
+        $ip=getenv('REMOTE_ADDR');
+    }else $ip="UNKNOWN";
+
+    return $ip;
+}
+function fb_auth($session,$helper){
 
     if ($session) {
         // Logged in
@@ -22,19 +80,17 @@ function fb_auth($session){
             $user_profile = (new FacebookRequest(
                 $session, 'GET', '/me'
             ))->execute()->getGraphObject(GraphUser::className());
-
-            echo "Name: " . $user_profile->getName();
-            echo "Name: " . $user_profile->getEmail();
-            echo "Name: " . $user_profile->getId();
-            echo "Name: " . $user_profile->getGender();
+            return $user_profile;
 
         } catch(FacebookRequestException $e) {
             echo "Exception occured, code: " . $e->getCode();
             echo " with message: " . $e->getMessage();
         }
+        return true;
     } else{
         $req_permissions=array('req_perms'=>'email');
-        echo '<a href="' . $helper->getLoginUrl($req_permissions) . '">Login with Facebook</a>';
+        $fb_auth_url=$helper->getLoginUrl($req_permissions);
+        header("Location: $fb_auth_url");
         try {
             $session = $helper->getSessionFromRedirect();
         } catch(FacebookRequestException $ex) {
